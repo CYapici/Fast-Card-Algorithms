1ba229c7554f1624bc39ca85242588d4c3e039c9
Cagatay Yapici <cagatayyapici@ip-192-168-20-115.eu-west-1.compute.internal>
2018-01-22 16:55:55 +0000

    code format
    code formatted

--- src/com/cagatayyapici/luhn/LuhnCheck.java
+++ src/com/cagatayyapici/luhn/LuhnCheck.java
@@ -3,7 +3,7 @@ package com.cagatayyapici.luhn;
 import java.math.BigInteger;
 
 /**
- * no String  * memory friendly implementation
+ * no String * memory friendly implementation
  * 
  * @author cagatayyapici
  *
@@ -14,20 +14,15 @@ public class LuhnCheck {
 		long value = num1.longValueExact();
 		int digitIdx = 0;
 		long digit;
-
 		do {
 			digit = value % 10;
 			if (++digitIdx % 2 == 0) {
-				digit = (digit <<= 1 % 10) + 1;
-				if (digit <= 9)
-					digit--;
+				long temp = (digit <<= 1 % 10);
+				digit = digit <= 9 ? temp : ++temp;
 			}
 			sum += digit;
-
 		} while ((value /= 10) > 0);
 		return (sum % 10 == 0);
 	}
-	
-	
-	
+
 }
