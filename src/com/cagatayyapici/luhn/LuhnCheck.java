3d59fd2325c160052332e77b46aa2239ebdebfee
Cagatay Yapici <cagatayyapici@ip-192-168-20-115.eu-west-1.compute.internal>
2018-01-26 17:44:39 +0000

    formatted & refactored
    formatted & refactored

--- src/com/cagatayyapici/luhn/LuhnCheck.java
+++ src/com/cagatayyapici/luhn/LuhnCheck.java
@@ -9,20 +9,20 @@ import java.math.BigInteger;
  *
  */
 public class LuhnCheck {
-	public static boolean Check(BigInteger num1) {
-		int sum = 0;
-		long value = num1.longValueExact();
-		int digitIdx = 0;
-		long digit;
-		do {
-			digit = value % 10;
-			if (++digitIdx % 2 == 0) {
-				long temp = (digit <<= 1 % 10);
-				digit = digit <= 9 ? temp : ++temp;
-			}
-			sum += digit;
-		} while ((value /= 10) > 0);
-		return (sum % 10 == 0);
-	}
+  public static boolean Check(BigInteger num1) {
+    int sum = 0;
+    long value = num1.longValueExact();
+    int digitIdx = 0;
+    long digit;
+    do {
+      digit = value % 10;
+      if ((++digitIdx & 1) == 0) {
+        long temp = (digit <<= 1 % 10);
+        digit = digit <= 9 ? temp : ++temp;
+      }
+      sum += digit;
+    } while ((value /= 10) > 0);
+    return (sum % 10 == 0);
+  }
 
 }
